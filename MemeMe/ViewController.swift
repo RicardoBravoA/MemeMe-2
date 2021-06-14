8247eb39b0fdfb9506e4afbff173783f170aa08e
Ricardo Bravo <ricardobravo@outlook.com>
2021-06-14 15:49:03 -0500

    Initial Commit

---
+++ MemeMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemeMe
+//
+//  Created by Ricardo Bravo on 14/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

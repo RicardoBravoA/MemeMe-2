3887a9f0759591056f9a886d5ea6901c5e76d755
Ricardo Bravo <ricardobravo@outlook.com>
2021-06-14 16:04:37 -0500

    feat: Add picker images from phone

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageViewPicker: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func pickImage(_ sender: Any) {
+        let pickerCOntroller = UIImagePickerController()
+        present(pickerCOntroller, animated: true, completion: nil)
+    }
+    
 }
 

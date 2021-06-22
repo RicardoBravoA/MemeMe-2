8ad39bae72a00cd76c45e9ee61b048fb402f0977
Ricardo Bravo <ricardobravo@outlook.com>
2021-06-21 19:11:11 -0500

    feat: Add collection view

--- MemeMe/Controller/MemeCollectionViewController.swift
+++ MemeMe/Controller/MemeCollectionViewController.swift
@@ -7,12 +7,57 @@
 
 import UIKit
 
-class MemeCollectionViewController: UICollectionViewController {
+class MemeCollectionViewController: UIViewController {
+    
+    @IBOutlet weak var memeCollectionView: UICollectionView!
+    @IBOutlet weak var flowLayout: UICollectionViewFlowLayout!
+    
+    let countries = [
+        Country(isoCode: "at", name: "Austria"),
+        Country(isoCode: "be", name: "Belgium"),
+        Country(isoCode: "de", name: "Germany"),
+        Country(isoCode: "el", name: "Greece"),
+        Country(isoCode: "fr", name: "France"),
+    ]
     
     var memes: [Meme]! {
         let object = UIApplication.shared.delegate
         let appDelegate = object as! AppDelegate
         return appDelegate.memes
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        memeCollectionView.delegate = self
+        memeCollectionView.dataSource = self
+        
+        let space:CGFloat = 5.0
+        let dimension = (view.frame.size.width - (2 * space)) / 3.0
+
+        flowLayout.minimumInteritemSpacing = space
+        flowLayout.minimumLineSpacing = space
+        flowLayout.estimatedItemSize = CGSize(width: dimension, height: dimension)
+    }
+    
+}
+
+extension MemeCollectionViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return countries.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = memeCollectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath) as! CollectionViewCell
+        let country = countries[indexPath.row]
+        
+        cell.memeImageView.image = UIImage(named: country.isoCode)
+        return cell
     }
     
 }
